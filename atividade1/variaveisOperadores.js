0a5ea5c77c7c15765d9ba7f5aeab8367a69eb571
Ana Luísa Da Penha Matos <113694235+analuisa2710@users.noreply.github.com>
2024-03-14 10:24:14 -0300

    atividade1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,18 @@
+const a = 6;
+const b = 6;
+let c;
+
+c = a +b;
+console.log (`o resultado da soma entre ${a} e ${b} é ${c}`);
+
+c = a - b;
+console.log (`o resultado da subtracao entre ${a} e ${b} é ${c}`);
+
+c = a * b;
+console.log (`o resultado da mutiplicacao entre ${a} e ${b} é ${c}`);
+
+c = a / b ;
+console.log (`o resultado da divisao entre ${a} e ${b} é ${c}`);
+
+
+
